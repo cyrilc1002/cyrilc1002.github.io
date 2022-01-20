a8ec9c313d5fcf01bc3705a24d9b6accf47d76ab
cyrilc1002 <91596220+cyrilc1002@users.noreply.github.com>
2022-01-20 10:29:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+let container, camera, renderer, scene, car, controls;
+
+function init() {
+	container = document.querySelector(".scene");
+  
+	scene = new THREE.Scene();
+  
+	const fov = 35;
+	const aspect = container.clientWidth / container.clientHeight;
+	const near = 1;
+	const far = 1000;
+
+	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	camera.position.set(0, 0, 30);
+  
+	const ambient = new THREE.AmbientLight(0x404040, 8);
+	scene.add(ambient);
+  
+	const light = new THREE.DirectionalLight(0xffffff, 10);
+	light.position.set(50, 50, 100);
+	scene.add(light);
+
+	renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+	renderer.setSize(container.clientWidth, container.clientHeight);
+	renderer.setPixelRatio(window.devicePixelRatio);
+  
+	container.appendChild(renderer.domElement);
+  
+
+	let loader = new THREE.GLTFLoader();
+	loader.load("./3d/scene.gltf", function(gltf) {
+	  scene.add(gltf.scene);
+	  car = gltf.scene.children[0];
+	  animate();
+	});
+  }
+  
+  function animate(){
+	  requestAnimationFrame( animate );
+	  car.rotation.z -= 0.01;
+	  renderer.render(scene, camera);
+  }
+  init();
+  
+  function onWindowResize() {
+	camera.aspect = container.clientWidth / container.clientHeight;
+	camera.updateProjectionMatrix();
+  }
